270b70b9669025c6a7a33e5e54b038dcd9ca38b6
Jeferson Chagas <jeferson_chagas25@hotmail.com>
2020-11-06 00:40:13 -0300

    projOperacoesPoliciais | criando o app diligencias

---
+++ diligencias/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from diligencias.models import Diligencia
+
+admin.site.register(Diligencia)
--- operacoes/settings.py
+++ operacoes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'enderecos',
     'pessoas',
+    'diligencias',
 ]
 
 MIDDLEWARE = [
