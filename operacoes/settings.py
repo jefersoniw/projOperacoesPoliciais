8c837c79674d1e84d0bfb741eb92650654a49985
Jeferson Chagas <jeferson_chagas25@hotmail.com>
2020-11-06 00:18:13 -0300

    projOperacoesPoliciais | criando o app pessoas, com mais de uma classe dentro do models e upload de fotos

--- operacoes/settings.py
+++ operacoes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'enderecos',
+    'pessoas',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#adicionar os dois abaixo
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
--- pessoas/admin.py
+++ pessoas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from pessoas.models import Pessoa, Telefone
+
+admin.site.register(Pessoa)
+admin.site.register(Telefone)
